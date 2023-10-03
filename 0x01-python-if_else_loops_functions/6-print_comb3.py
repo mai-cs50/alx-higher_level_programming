1335bf060636fe4a88ab34fe72f385de7ddd1689
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-03 12:28:58 -0700

    s

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for n1 in range(10):
     for n2 in range(n1, 10):
         if n1 < n2:
-            print("{:d}{:d}".format(n1, n2)
-                 , end="\n" if n1 == 8 and n2 == 9 else ", ")
+            print("{:d}{:d}".format(n1, n2),
+                  end="\n" if n1 == 8 and n2 == 9 else ", ")
