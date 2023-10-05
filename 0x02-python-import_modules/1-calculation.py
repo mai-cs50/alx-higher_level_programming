36f0eb2d91c33c1f75a9e1976fe174fb6b8c6e47
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-05 09:07:15 -0700

    s

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name = "__main__":
-    from calculator_1 import add,sub,dev,mul
+    from calculator_1 import add, sub, dev, mul
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a,b)))
-    print("{} - {} = {}".format(a, b, sub(a,b)))
-    print("{} * {} = {}".format(a, b, mul(a,b)))
-    print("{} / {} = {}".format(a, b, dev(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, dev(a, b)))
