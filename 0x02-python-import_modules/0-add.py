0dffc366dc10ac902c2531550a10bccd926bb1e6
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-05 05:08:39 -0700

    a

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
-from add_0 import add
-a = 1
-b = 2
-add(a,b)
-print(f"a + b =" {add})
+if __name__ == "__main__":    
+    from add_0 import add
+    a = 1
+    b = 2
+    add(a,b)
+    print("{} + {} = {}".format(a, b, add(a, b)))
