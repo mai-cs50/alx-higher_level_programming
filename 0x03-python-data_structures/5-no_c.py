47056ddbda2d7ee2cc21af8a012e1f1d0ba8b7d3
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-07 09:47:37 -0700

    s

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,6 @@
 def no_c(my_string):
     r = ""
     for i in range(len(my_string)):
-        if my_string[i] != "C" or my_string[i] != "c":
-            return(r += my_string[i])
+        if my_string[i] != "C" and my_string[i] != "c":
+            r += my_string[i]
+    return r
