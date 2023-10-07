442191098c015836b99423816bd7ae8ffb198575
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-07 14:19:47 -0700

    s

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) < 0 or not sentence:
-        return None
-    else:
+    if len(sentence) > 0:
         return(len(sentence), sentence[0])
+    else:
+        return None
