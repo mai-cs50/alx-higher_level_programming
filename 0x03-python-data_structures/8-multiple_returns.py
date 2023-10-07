7ac609f08af8c87b972d894d76f0c563c0d31039
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-07 10:17:38 -0700

    s

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        return None
+    else:
+        length = len(sentence)
+        return length
+        first = sentence[0]
+        return first
