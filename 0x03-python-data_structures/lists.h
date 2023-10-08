838407ed6b14b9dad64883fdd5f9608788c33bb3
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-08 10:39:39 -0700

    s

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -23,6 +23,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int aux_palind(listint_t **head, listint_t *end)
+int aux_palind(listint_t **head, listint_t *end);
 
 #endif /* LISTS_H */
