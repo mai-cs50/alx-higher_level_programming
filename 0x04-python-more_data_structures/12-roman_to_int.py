a76625e8b262d12b61594d2b77d5863f5cec2678
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-11 11:59:38 -0700

    a

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    t = 0
+    n = 0
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in reversed(roman_string):
+        n = digits[i]
+        t += n if t < n * 5 else -n
+    return t
