b7b63edf85bf25d3dd58829e47e36cc703266ca4
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-23 23:03:08 -0700

    d

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
             i += 1
-        except IndexError:
+    except IndexError:
             pass
-        finally:
-            print()
-            return i
+    finally:
+        print()
+        return i
