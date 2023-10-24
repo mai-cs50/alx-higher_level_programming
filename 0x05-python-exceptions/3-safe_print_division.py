e6d069d60025079b4f5c9b873d6ce4a4f4348eb2
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-23 23:07:12 -0700

    d

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,6 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
-    finally:print("Inside result: {}".format(result))
+    finally:
+        print("Inside result: {}".format(result))
     return result
