e78a53a997a767b44952cf4ad7a6e8377ea3f2cc
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-29 12:53:47 -0700

    d

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_square(size):
+    if isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print ((("#" * size + "\n") * size), end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
