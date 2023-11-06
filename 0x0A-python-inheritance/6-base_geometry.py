3045a15d14d5499f03e834860a9c08dcf8668f52
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-06 12:59:57 -0800

    d

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Write a class BaseGeometry (based on 5-base_geometry.py).'''
+
+
+class BaseGeometry:
+    '''class BaseGeometry'''
+    def area(self):
+        ''' that raises an Exception with
+        the message area() is not implemented'''
+        raise Exception('area() is not implemented')
