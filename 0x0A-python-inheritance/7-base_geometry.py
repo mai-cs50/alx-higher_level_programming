16ab80f9f99dfa1221816616d9b88c545ed73921
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-06 13:16:34 -0800

    d

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' class BaseGeometry (based on 6-base_geometry.py)'''
+
+
+class BaseGeometry:
+    '''method to compute this area'''
+    def area(self):
+        '''method to compute area'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        '''method for validating value'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raisse ValueError("{} must be greater than 0".format(name))
+
+
