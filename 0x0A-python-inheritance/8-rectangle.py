ad41d060c1695d203364cf1f873a4532597358ff
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-06 13:29:01 -0800

    d

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''class Rectangle that inherits from BaseGeometry'''
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle:
+    '''a subclass represinting a rectangle'''
+    def __init__(self, width, height):
+        '''constructors'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
