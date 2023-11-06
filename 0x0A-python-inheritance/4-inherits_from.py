f087fe5456d0a0c64171798633f54a2afb55b1db
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-06 12:52:52 -0800

    d

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''returns True if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class ; otherwise False.'''
+
+
+def inherits_from(obj, a_class):
+    '''inherits_from'''
+    return isinstance(obj, a_class) and type(obj) != a_class
