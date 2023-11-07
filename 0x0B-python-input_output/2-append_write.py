a53b8b3c3c37d00b6805237da79fb5f4707e174b
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-07 08:28:35 -0800

    f

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 '''Write a function that appends a string at the end of a text file'''
 
 
-def append_file(filename="", text=""):
+def append_write(filename="", text=""):
     '''read file name with utf-8'''
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
