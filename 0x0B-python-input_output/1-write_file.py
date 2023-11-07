6967c5010518641eebe1bd48ff143c6ba6e731ac
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-07 08:17:16 -0800

    f

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    '''read file name with UTF8'''
+    '''read file name with utf-8'''
     with open(filename, "w", endcoding="utf-8") as f:
         return f.write(text)
