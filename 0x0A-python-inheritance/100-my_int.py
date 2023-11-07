1826a65d5d0e1a2f8b34c9d1762fb7aea51ba86f
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-06 23:24:34 -0800

    s

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-'''class MyInt that inherits from int'''
+'''class MyInt that inherits from int
+'''
 
 
 class MyInt(int):
-    ''' '''
+    '''rebel version of unteger'''
     def __new__(cls, *args, **kwargs):
-        ''' '''
+        '''instant of class'''
         return super(MyInt, cls).__new__(cls, *args, **kwargs)
 
     def __eq__(self, other):
-        ''' '''
+        '''MyInt has == and != operators inverted'''
         return int(self) != other
 
     def __ne__(self, other):
-        ''' '''
+        '''MyInt has == and != operators inverted'''
         return int(self) == other
