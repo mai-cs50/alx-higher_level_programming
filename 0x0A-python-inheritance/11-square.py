67fef6ee95905c0bfca7400d88d18d3629aa21eb
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-06 23:12:13 -0800

    s

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''module of rectangle class'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''square subclass'''
+    def __init__(self, size):
+        '''constructor'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''method area of square'''
+        return self.__size ** 2
+    def __str__(self):
+        '''return string'''
+        return "[Square] " + str(self.__size) + "/" + str(self.size)
