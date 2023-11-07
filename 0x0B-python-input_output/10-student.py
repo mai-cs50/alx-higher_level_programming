c6da6607ac75ac883a482e047d0a4207207b23f4
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-07 09:37:07 -0800

    f

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Write a class Student that defines a student by'''
+
+
+class Student:
+    '''base class'''
+    def __init__(self, first_name, last_name, age):
+        '''Instantiation student'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''retrieves a dictionary representation of a Student instance'''
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
