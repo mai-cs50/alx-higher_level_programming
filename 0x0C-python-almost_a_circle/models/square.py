476d791a3a30bf32e10d3841b66b4173c432bdaf
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-10 09:36:52 -0800

    e

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,21 @@ class Square:
     def size(self, value):
         self.width = width
         self.height = height
+
+    def update(self, id=None, size=None, x=None, y=None):
+        '''method that assigns attributes'''
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''update via no key-word'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
