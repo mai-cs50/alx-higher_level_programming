b9581474e6dfc31cf66e3cee5c2d1f2062663ff9
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-10 10:01:37 -0800

    e

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''jsonifiles Dictionary'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
