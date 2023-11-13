4e5409ac851cb2177d1c77c9f3c96ffa371452d7
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-13 11:16:11 -0800

    s

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,12 +3,12 @@
 from models.rectangle import Rectangle
 
 
-class Square:
+class Square(Rectangle):
     '''Square class'''
 
     def __init__(self, size, x=0, y=0, id=None):
         '''constuctor'''
-        super().__init__(self, size, x=0, y=0, id=None)
+        super().__init__(self, size, x, y, id)
 
     def __str__(self):
         '''return str info about square'''
