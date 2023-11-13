d689dbfcb97323ab78259f4904a8559909932b39
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-13 11:04:11 -0800

    s

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,87 +15,87 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            '''rectangle width'''
-            return self.__width
+    @property
+    def width(self):
+        '''rectangle width'''
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        self.__width = value
 
-        @property
-        def height(self):
-            '''rectangle height'''
-            retur self.__height
+    @property
+    def height(self):
+        '''rectangle height'''
+        retur self.__height
 
-        @height.setter
-        def height(self, value):
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        self.__height = value
 
-        @property
-        def x(self):
-            '''x rectangle'''
-            return self.__x
+    @property
+    def x(self):
+        '''x rectangle'''
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        self.__x = value
 
-        @property
-        def y(self):
-            '''y rectangle'''
-            return self.__y
+    @property
+    def y(self):
+        '''y rectangle'''
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        self.__y = value
 
-        def validate_integer(self, name, value, eq=True):
-            '''method of validate'''
-            if type(value) != int:
-                raise TypeError("{} must be an integer".format(name))
-            if eq and value < 0:
-                raise ValueError("{} must be >= 0".format(name))
-            elif not eq and value <= 0:
-                raise ValueError("{} must be > 0".format(name))
+    def validate_integer(self, name, value, eq=True):
+        '''method of validate'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
 
-        def area(self):
-            '''area of rectangle'''
-            return self.width * self.height
+    def area(self):
+        '''area of rectangle'''
+        return self.width * self.height
 
-        def display(self):
-            '''prints in stdout the Rectangle instance with the character #'''
-            s = '\n' * self.y + (' ' * self.x + '#' * self.width + '\n') * self.height
-            print(s, end="")
+    def display(self):
+        '''prints in stdout the Rectangle instance with the character #'''
+        s = '\n' * self.y + (' ' * self.x + '#' * self.width + '\n') * self.height
+        print(s, end="")
 
-        def __str__(self):
-            '''return str info of rectangle'''
-            return'[{}] ({}) {}/{} - {}/{}'.format(type(self).\
-                    __name__, self.id, self.x, self.y, self.width, self.height))
+    def __str__(self):
+        '''return str info of rectangle'''
+        return'[{}] ({}) {}/{} - {}/{}'.format(type(self).\
+                __name__, self.id, self.x, self.y, self.width, self.height))
 
-        def update(self, id=None, width=None, height=None, x=None, y=None):
-            '''update instance attr'''
-            if id is not None:
-                self.id = id
-            if width is not None:
-                self.width = width
-            if height is not None:
-                self.height = height
-            if x is not None:
-                self.x = x
-            if y is not None:
-                self.y = y
+    def update(self, id=None, width=None, height=None, x=None, y=None):
+        '''update instance attr'''
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
 
-        def update(self, *args, **kwargs):
-            '''update via no-keyword'''
-            if args:
-                self.__update(*args)
-            elif kwargs:
-                self.__update(**kwargs)
+    def update(self, *args, **kwargs):
+        '''update via no-keyword'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
 
-        def to_dictionary(self):
-            '''return dictionary represintation'''
-            return {"id": self.id, "width": self.width, "height": self.height,
-                    "x": self.x, "y": self.y}
+    def to_dictionary(self):
+        '''return dictionary represintation'''
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
 
