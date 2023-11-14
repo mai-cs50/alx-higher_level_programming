b6815b27a304090c21715969b83342cf408b2bca
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-14 08:52:04 -0800

    s

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -70,14 +70,14 @@ class Rectangle(Base):
     def display(self):
         '''prints in stdout the Rectangle instance with the character #'''
         s = '\n' * self.y +\
-                (' ' * self.x + '#' * self.width + '\n') * self.height
+            (' ' * self.x + '#' * self.width + '\n') * self.height
         print(s, end="")
 
     def __str__(self):
         '''return str info of rectangle'''
-        return'[{}] ({}) {}/{} - {}/{}'.\
-                format(type(self).__name__,self.id, self.x, self.y, self.width,
-                       self.height)
+        return '[{}] ({}) {}/{} - {}/{}'.\
+            format(type(self).__name__,self.id, self.x, self.y, self.width,
+                   self.height)
 
     def update(self, id=None, width=None, height=None, x=None, y=None):
         '''update instance attr'''
