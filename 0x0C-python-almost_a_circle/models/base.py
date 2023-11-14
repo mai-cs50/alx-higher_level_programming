bd051bfa1327174ded13ddaa20c8da2f92d1df97
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-14 08:42:21 -0800

    s

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -77,7 +77,7 @@ class Base:
                 list_objs = [[o.id, o.size, o.x, o.y]
                              for o in list_objs]
         with open('{}.csv'.format(cls.__name__), 'w', newline='',
-                encoding='utf-8') as f:
+                  encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerows(list_objs)
 
@@ -88,17 +88,17 @@ class Base:
         from models.square import Square
         ret = []
         with open('{}.csv'.format(cls.__name__), 'r', newline='',
-                encoding='utf-8') as f:
+                  encoding='utf-8') as f:
             reader = csv.reader(f)
             for row in reader:
                 row = [int(r) for r in row]
                 if cls is Rectangle:
                     d = {"id": row[0], "width": row[1], "height": row[2],
-                            "x": rowp[3], "y": row[4]}
+                         "x": rowp[3], "y": row[4]}
                 else:
                     d = {"id": row[0], "size": row[1],
-                            "x": row[2], "y": row[3]}
-                ret.append(cls.creat(**d))
+                         "x": row[2], "y": row[3]}
+                ret.append(cls.create(**d))
         return ret
 
     @staticmethod
