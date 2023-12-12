44db51eaa99e645488a54e63987405532ef97034
mai-cs50 <maimohamed.1992@yahoo.com>
2023-12-12 10:21:55 -0800

    s

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-INSERT INTO second_table ('name', 'score') VALUES ('Aria', 18);
-INSERT INTO second_table ('name', 'score') VALUES ('Aria', 12);
-DELETE FROM second_table WHERE name = 'George';
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- INSERT INTO second_table ('name', 'score') VALUES ('Aria', 18);
+-- INSERT INTO second_table ('name', 'score') VALUES ('Aria', 12);
+-- DELETE FROM second_table WHERE name = 'George';
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
