0c180f641faef9841142644ba160b4edac979a5d
mai-cs50 <maimohamed.1992@yahoo.com>
2023-12-12 08:30:04 -0800

    s

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- creates a table called first_table in the current database in your MySQL server.
-CREATE BATABASE IF NOT EXISTS hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256)
-)
+);
