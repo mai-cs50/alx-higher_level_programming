66ecbdc1c1d7f41717213c23f5a2210657a04abe
mai-cs50 <maimohamed.1992@yahoo.com>
2023-12-13 09:50:45 -0800

    w

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- creates the table unique_id on your MySQL server.
-CREATE TABLE IF NOT EXISTS unique_id (id INT NOT NULL BRIMARY KEY, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT NOT NULL BRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
