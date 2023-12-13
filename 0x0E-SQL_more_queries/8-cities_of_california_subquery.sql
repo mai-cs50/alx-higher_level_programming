31450060db06244034c7453fe3d63177b44024e7
mai-cs50 <maimohamed.1992@yahoo.com>
2023-12-13 11:05:27 -0800

    w

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
 SELECT * FROM cities
-WHERE name = California ORDER BY id ASC;
+WHERE name = California ORDER BY cities.id ASC;
