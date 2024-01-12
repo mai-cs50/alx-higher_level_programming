a8057ea7c8590e63075c63478fb1bcb578a8bce6
mai-cs50 <maimohamed.1992@yahoo.com>
2024-01-12 09:37:31 -0800

    s

---
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -0,0 +1,9 @@
+--lists all states from the database hbtn_0e_0_usa--
+CREATE DATABASE IF NOT EXISTS
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states (
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
