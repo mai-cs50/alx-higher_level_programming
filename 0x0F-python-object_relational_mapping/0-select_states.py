4f1e3eec63acb75f8ed265d7d397cf0e6aff8f7c
mai-cs50 <maimohamed.1992@yahoo.com>
2024-01-12 09:45:28 -0800

    s

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 '''lists all states from the database hbtn_0e_0_usa'''
 
 import MySQLdb
-    from sys import argv
+from sys import argv
 
 if __name__ = "__main__":
     con = MySQLdb.connect(host = "localhost", port = 3306, user = argv[1],
