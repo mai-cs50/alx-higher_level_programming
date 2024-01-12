3667ad4c8200accad0d082897021812005da386c
mai-cs50 <maimohamed.1992@yahoo.com>
2024-01-12 09:57:23 -0800

    s

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,10 +7,10 @@ from sys import argv
 if __name__ = "__main__":
     con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                           passwd=argv[2], db=argv[3], charset="utf8")
-cur = con.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-con.close()
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    con.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,10 +8,12 @@ if __name__ = "__main__":
     con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                           passwd=argv[2], db=argv[3], charset="utf8")
     cur = con.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        if row[1].startswith("N"):
-            print(row)
-    cur.close()
-    con.close()
+    query = """
+SELECT
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+query_rows = cur.fetchall()
+for row in query_rows:
+    if row[1].startswith("N"):
+        print(row)
+cur.close()
+con.close()
