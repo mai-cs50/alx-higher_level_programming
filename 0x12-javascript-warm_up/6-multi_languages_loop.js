86cbb56d82149f430eb332d3b3e523c862b09178
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-12 11:11:25 -0800

    s

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 //rints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
 const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let index = 0; index < leng.length; index++) {
+for (let index = 0; index < lang.length; index++) {
 	cosole.log(lang[index]);
 }
