6891a50f13407f727877d97c09de7f100275bef6
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-12 11:12:44 -0800

    s

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 //script that prints x times “C is fun”
 
-const l = 'C is fun';
+const x = 'C is fun';
 
 if (isNaN(process.argv[2])) {
 	console.log('Missing number of occurrences');
 } else {
 	for (let i = 0; i < parseInt(process.argv[2]); i++){
-		console.log(l);
+		console.log(x);
 	}
 }
