1cbaa2e171e75e1457451dfcc42d1a311f2e092c
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-12 10:32:21 -0800

    a

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let index = 0; index < myArr.length; index++) {
-	cosole.log(myArr[index]);
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < leng.length; index++) {
+	cosole.log(lang[index]);
 }
