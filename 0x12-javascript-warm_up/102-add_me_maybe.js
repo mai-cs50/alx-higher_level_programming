bdedc99bde6f562dd6f0c8b4632f020312c7c9e7
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-13 12:24:15 -0800

    w

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-//Write a file that modifies the value of myVar to 333
+// Write a file that modifies the value of myVar to 333
 
 exports.addMeMaybe = function (number, theFunction) {
-	number++;
-	theFunction(number);
+  number++;
+  theFunction(number);
 };
