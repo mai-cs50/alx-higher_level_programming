70cfe3c6575609c591f89af7898ffba7a4a22338
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-13 12:08:51 -0800

    w

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//	rints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+// rints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
 
 const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
