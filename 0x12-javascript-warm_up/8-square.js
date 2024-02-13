e4269a0fc7661f1fb583c5a0b63e632c62521af1
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-13 12:18:33 -0800

    w

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-//script that prints a square
+// script that prints a square
 
 if (isNaN(process.argv[2])) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	for (let i = 0; i < parseInt(process.argv[2]); i++) {
-		console.log('X'.repeat(parseInt(process.argv[2])));
-	}
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+  }
 }
