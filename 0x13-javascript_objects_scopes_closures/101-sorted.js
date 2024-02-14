188a1f6195539e15d3e515f0f3d304e4025a1bc1
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-14 10:58:34 -0800

    s

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-exports.dict = {
-	89: 1,
-	90: 2,
-	91: 1,
-	92: 3,
-	93: 1,
-	94: 2
-};
+// script that imports an array and computes a new array.
+
+const dict = require('./101-data').dict;
+let newDict = {};
+for (let userId in dict) {
+  let occurence = dict[userId];
+  if (!newDict[occurence]) {
+    newDict[occurence] = [];
+  }
+  newDict[occurence].push(userId);
 }
+console.log(newDict);
