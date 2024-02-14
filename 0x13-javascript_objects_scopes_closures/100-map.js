7970eb017f6f62f9b9a16573648a84744149ef1c
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-14 10:34:14 -0800

    s

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-const { list } = require('./100-data.js')
+// script that imports an array and computes a new array.
+
+const { list } = require('./100-data').list;
 console.log(list);
 console.log(list.map((element, idx) => element * idx));
