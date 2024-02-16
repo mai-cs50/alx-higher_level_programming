2193a1e0845030a623bbf292967752eb4f041f3a
mai-cs50 <maimohamed.1992@yahoo.com>
2024-02-16 08:08:19 -0800

    s

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+write a script that takes in arguments and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!
+'''
+
+import MySQLdb
+from model_state import Base, State
+from SQLAlchemy import creat_engine
+from SQLAlchemy.orm import sessionmaker
+
+if __name__=="__main__":
+    engine = creat_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                          .format(argv[1], argv[2], argv[3]),
+                          pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+    session.close()
