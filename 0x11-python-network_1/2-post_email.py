97c6c4593090ef1f5c1edacf3a4ad8a58efc3289
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-01 11:26:43 -0800

    sw

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
+
+    with urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
