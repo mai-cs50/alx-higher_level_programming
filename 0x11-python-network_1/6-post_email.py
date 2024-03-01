f2217ad722ba3cb73b773ce2e3fe555e8af287f8
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-01 11:49:06 -0800

    sw

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    req = requests.post(url, data=value)
+
+    print(req.text)
