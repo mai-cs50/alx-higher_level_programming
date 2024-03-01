dc366ce0357fd4d0874279d9afdf479723fff909
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-01 11:51:24 -0800

    sw

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = requests.get(url)
+
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
