45734df204a796d42ae406ce2057c81f461ddce0
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-01 11:47:19 -0800

    sw

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = requests.get(url)
+
+    print(req.headers.get("X-Request-Id"))
