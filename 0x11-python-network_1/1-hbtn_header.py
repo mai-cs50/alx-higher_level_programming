633d05861acf1797ce1774433696d20a69015a72
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-01 11:19:20 -0800

    sw

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,6 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        html = response.info()
-        value = html.get('X-Request-Id')
-        print(value)
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader(''X-Request-Id''))
