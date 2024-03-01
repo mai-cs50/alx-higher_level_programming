e1decb321a59788d2e8706d7e0acd73ead82d855
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-01 08:48:47 -0800

    s

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
+curl -s "$1" | wc -c
