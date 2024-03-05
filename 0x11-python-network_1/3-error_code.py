ea49d1f2a6e173a5abd804fc926ef7d987d4c0eb
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-05 13:49:51 -0800

    d

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)'''
+
+
 import urllib.request
 import urllib.parse
 from urllib.request import Request, urlopen
