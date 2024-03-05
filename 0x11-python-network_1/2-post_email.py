f364a91f3fe3088b7436e41bc66192a8b626ea63
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-05 13:48:41 -0800

    d

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)'''
+
+
 import urllib.request
 import urllib.parse
 from urllib.request import Request, urlopen
