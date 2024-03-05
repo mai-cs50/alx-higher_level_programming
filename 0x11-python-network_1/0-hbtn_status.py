aca74ec433e7fe262e846d28e60d8574494fedff
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-05 13:42:31 -0800

    d

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,11 @@
 from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
-    req = Request("https://alx-intranet.hbtn.io/status")
+    url = "http://0.0.0.0:5050/status"
+    req = requests.get(url)
     with urlopen(req) as response:
         body = response.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(body)))
-        print('\t- content: {}' .format(body))
-        print('\t- utf8 content: {}'.format(body.decode("utf-8")))
+        print('\t- type: {}'.format(type(req.text)))
+        print('\t- content: {}' .format(req.text))
+        #print('\t- utf8 content: {}'.format(body.decode("utf-8")))
