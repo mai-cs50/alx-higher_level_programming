1eba2afece63decc85cb6fa624378bf638fdc3dc
mai-cs50 <maimohamed.1992@yahoo.com>
2024-03-05 13:50:58 -0800

    d

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''Write a Python script that takes in a URL, sends a request to the URL
+and displays the value of the variable X-Request-Id in the response header'''
+
+
 import requests
 from sys import argv
 
