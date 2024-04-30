ff468816bcbfd1763a4782a996c4c3a602fd1285
mai-cs50 <maimohamed.1992@yahoo.com>
2024-04-30 12:46:58 -0700

    w

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,17 +3,12 @@
 Write a python file that contains the class definition of a State and an instance Base = declarative_base():
 '''
 
-from SQLAlchemy import Column, Integer, String
-from SQLAlchemy.ext.declarative import declarative_base
+from sys import argv
+from model_state import Base, state
+from SQLAlchemy import (create_engine)
 
 if __name__ == "__main__":
-    Base = declarative_base()
-
-    class State(Base):
-        '''
-        state class
-        '''
-
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-        name = Column(String(128), nullable=False)
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
