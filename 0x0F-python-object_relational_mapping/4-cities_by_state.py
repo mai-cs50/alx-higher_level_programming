f2169f3fe98ae9e097f6ffeb050f030a2174e39b
mai-cs50 <maimohamed.1992@yahoo.com>
2024-04-30 12:11:37 -0700

    w

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,7 @@ import MySQLdb
 from sys import argv
 
 if __name__=="__main__":
-    db = MySQLdb.connect(host="localhost",
+    db = MySQLdb.connect (host="localhost",
                          user=argv[1], passwd=argv[2], db=argv[3])
     query = "SELECT cities.id, cities.name, states.name\
             FROM cities JOIN states ON cities.state_id = states.id\
