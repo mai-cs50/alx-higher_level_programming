d6740714853a6b969c240ea7593b46104b5c9731
mai-cs50 <maimohamed.1992@yahoo.com>
2024-04-30 13:50:09 -0700

    w

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -5,7 +5,7 @@ in the states table of hbtn_0e_0_usa where name matches the argument.
 But this time, write one that is safe from MySQL injections!
 '''
 
-import MySQLdb
+import sys
 from model_state import Base, State
 from SQLAlchemy import creat_engine
 from SQLAlchemy.orm import sessionmaker
@@ -14,11 +14,12 @@ if __name__=="__main__":
     engine = creat_engine('mysql+mysqldb://{}:{}@localhost/{}'
                           .format(argv[1], argv[2], argv[3]),
                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name == argv[4]).first()
-    if state:
-        print("{}".format(state.id))
+    states = session.query(State).filter(State.name == argv[4]).order_by(State.id).all()
+    if states:
+        print("{}".format(states[0].id))
     else:
         print("Not found")
     session.close()
