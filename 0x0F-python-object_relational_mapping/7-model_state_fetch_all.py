79b1db3d97cdc6dcd7c6ecbaa0a73e3af70bc474
mai-cs50 <maimohamed.1992@yahoo.com>
2024-04-30 13:07:58 -0700

    w

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,18 +5,19 @@ in the states table of hbtn_0e_0_usa where name matches the argument.
 But this time, write one that is safe from MySQL injections!
 '''
 
-import MySQLdb
+import sys import argv
 from model_state import Base, State
 from SQLAlchemy import creat_engine
 from SQLAlchemy.orm import sessionmaker
 
 if __name__=="__main__":
-    engine = creat_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                          .format(argv[1], argv[2], argv[3]),
+    engine = creat_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}',
                           pool_pre_ping=True)
     Base.metadata.creat_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    states = session.query(state).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
     session.close()
