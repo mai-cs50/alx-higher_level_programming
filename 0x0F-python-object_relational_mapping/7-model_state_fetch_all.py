33b91d5ebd6c4566f3a7114acda748e48d680dbe
mai-cs50 <maimohamed.1992@yahoo.com>
2024-04-30 13:13:08 -0700

    w

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,7 +10,7 @@ from model_state import Base, State
 from SQLAlchemy import creat_engine
 from SQLAlchemy.orm import sessionmaker
 
-if __name__=="__main__":
+if __name__ == "__main__":
     engine = creat_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}',
                           pool_pre_ping=True)
     Base.metadata.creat_all(engine)
