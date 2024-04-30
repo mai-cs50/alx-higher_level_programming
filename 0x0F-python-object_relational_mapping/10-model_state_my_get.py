02ef349a0f846aede67f96a86bcc0a1dccbaa1e5
mai-cs50 <maimohamed.1992@yahoo.com>
2024-04-30 13:52:28 -0700

    w

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,14 +10,15 @@ from model_state import Base, State
 from SQLAlchemy import creat_engine
 from SQLAlchemy.orm import sessionmaker
 
-if __name__=="__main__":
+if __name__ == "__main__":
     engine = creat_engine('mysql+mysqldb://{}:{}@localhost/{}'
                           .format(argv[1], argv[2], argv[3]),
                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name == argv[4]).order_by(State.id).all()
+    states = session.query(State)./
+    filter(State.name == argv[4]).order_by(State.id).all()
     if states:
         print("{}".format(states[0].id))
     else:
