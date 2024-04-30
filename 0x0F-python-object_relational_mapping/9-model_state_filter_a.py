78e8eba761c38692e2eecca7668b42602a568f95
mai-cs50 <maimohamed.1992@yahoo.com>
2024-04-30 13:30:36 -0700

    w

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,8 +14,9 @@ if __name__=="__main__":
     engine = creat_engine('mysql+mysqldb://{}:{}@localhost/{}'
                           .format(argv[1], argv[2], argv[3]),
                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter(State.name.like('%a%'))order_by(State.id).all():
+    for state in session.query(State).filter(State.name.like('%a%')).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
