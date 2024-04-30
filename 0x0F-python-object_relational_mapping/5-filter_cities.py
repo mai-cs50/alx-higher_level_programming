4238d3a805be4027db88e55161d34ab86262496c
mai-cs50 <maimohamed.1992@yahoo.com>
2024-04-30 12:21:57 -0700

    w

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__=="__main__":
     query = "SELECT cities.name\
             FROM cities JOIN states\
             ON cities.state_id = states.id\
-            WHERE states.name = %s
+            WHERE states.name = %s\
             ORDER BY cities.id ASC"
 
     cursor.execute(query, (argv[4], ))
