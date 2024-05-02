34a74f613ff67522c3ac44b12448b24d3155fc63
mai-cs50 <maimohamed.1992@yahoo.com>
2024-05-02 12:54:29 -0700

    a

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,6 +5,6 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort()(( a + b ) => b - a);
+  const list = process.argv.sort();
   console.log(list.reverse[1]);
 }
