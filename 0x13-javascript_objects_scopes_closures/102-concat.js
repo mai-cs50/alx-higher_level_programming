c6271f709108bb4a9f250c7b956e04dadff34020
mai-cs50 <maimohamed.1992@yahoo.com>
2024-05-02 13:39:15 -0700

    a

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,6 +3,6 @@
 
 const args = process.argv.slice(2);
 const file = require('fs');
-const contentA = file.readfileSync('./' + args[0]);
-const contentB = file.readfileSync('./' + args[1]);
-file.writefileSync('./' + args[2], contentA + contentB);
+const contentA = file.readfileSync(args[0] + 'utf8');
+const contentB = file.readfileSync(args[1] + 'utf8');
+file.writefileSync(args[2], contentA + contentB);
