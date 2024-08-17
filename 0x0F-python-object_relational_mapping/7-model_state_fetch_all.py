20d0b0b03a2a5662d077fa9abc8357bf0577e946
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 07:40:31 -0700

    d

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,8 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     # Create the engine and connect to the database
     engine = create_engine(
-            f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
+            f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/'
+            '{sys.argv[3]}',
             pool_pre_ping=True)
 
     # Create the tables if they don't exist
