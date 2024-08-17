323df2f940f61231bc190779dba958c8c7f1d568
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 07:31:31 -0700

    d

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
 '''
-write a script that takes in arguments and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
-But this time, write one that is safe from MySQL injections!
+Script that lists all State objects from the database hbtn_0e_6_usa
 '''
 
-import sys import argv
+import sys
 from model_state import Base, State
-from SQLAlchemy import creat_engine
-from SQLAlchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    #create_engine
-    engine = creat_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
-                          pool_pre_ping=True)
-    Base.metadata.creat_all(engine)
-    #creat
+    # Create the engine and connect to the database
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
+                           pool_pre_ping=True)
+    
+    # Create the tables if they don't exist
+    Base.metadata.create_all(engine)
+    
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(state).order_by(State.id).all()
+
+    # Query to fetch all State objects ordered by id
+    states = session.query(State).order_by(State.id).all()
+    
+    # Display the results
     for state in states:
         print(f"{state.id}: {state.name}")
 
+    # Close the session
     session.close()
+
