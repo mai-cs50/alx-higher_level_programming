b14d9b06c4dff31ec7829dbebda63b1a68898667
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 07:33:13 -0700

    d

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,23 +10,23 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     # Create the engine and connect to the database
-    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
+    engine = create_engine(f'mysql+mysqldb:
+            //{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
                            pool_pre_ping=True)
     
     # Create the tables if they don't exist
     Base.metadata.create_all(engine)
-    
+
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query to fetch all State objects ordered by id
     states = session.query(State).order_by(State.id).all()
-    
+
     # Display the results
     for state in states:
         print(f"{state.id}: {state.name}")
 
     # Close the session
     session.close()
-
