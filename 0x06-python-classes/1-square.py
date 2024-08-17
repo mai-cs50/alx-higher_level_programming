eda83624deea72fe0eded8759011d38e59eef065
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 04:22:17 -0700

    d

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+'''
+defines a square by: (based on 0-square.py)
+'''
+
+
 class Square:
+    '''
+    init
+    '''
     def __init__(self, size):
         '''d'''
         self.__size = size
