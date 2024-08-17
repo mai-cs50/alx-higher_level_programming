30fb6c4ee116671b14018b0562ab0270bed037e3
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 07:23:52 -0700

    d

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python3
-'''
-Write a python file that contains the class definition of a State and an instance Base = declarative_base():
-'''
+"""
+Defines a State class and an instance Base = declarative_base().
+"""
 
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sys import argv
-from model_state import Base, state
-from SQLAlchemy import (create_engine)
+
+# Create an instance of the Base class
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class that inherits from Base.
+    Represents a state in the states table in MySQL.
+    """
+    __tablename__ = 'states'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
+    # Create the engine and connect to the MySQL server
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'
-            .format(argv[1], argv[2],
-                    argv[3]), pool_pre_ping=True)
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True
+    )
+    
+    # Create all tables in the engine
     Base.metadata.create_all(engine)
+
