0ddf23c297bbad3e7b3f5dbbf02984c2b9aef9cb
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 04:55:51 -0700

    d

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-'''lists all states from the database hbtn_0e_0_usa'''
-
+# Displays all values in the states table of the database hbtn_0e_0_usa
+# whose name matches that supplied as argument.
+# Usage: ./2-my_filter_states.py <mysql username> \
+#                                <mysql password> \
+#                                <database name> \
+#                                <state name searched>
+import sys
 import MySQLdb
-from sys import argv
 
-# the code
 if __name__ == "__main__":
-    if len(argv) == 5:
-        username, password, database, state_name = argv[1],
-        argv[2], argv[3], argv[4]
-
-        # make connection
-        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3])
-
-        # give
-        cur = db.cursor()
-        me = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
-        cur.execute(me)
-
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        # clean
-        cur.close()
-        db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
