8e197bd75bbee668354b9f03e333e1ea770aaafd
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 08:41:15 -0700

    d

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 """
-Fetches all City objects from the database hbtn_0e_14_usa
-and prints them in the format: <state name>: (<city id>) <city name>
+Module to define the City class for SQLAlchemy ORM.
+
+The City class represents the 'cities' table in the database and has
+a relationship with the 'states' table.
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./14-model_city_fetch_by_state.py <mysql username> <mysql password> <database name>")
-        sys.exit(1)
+class City(Base):
+    """
+    Represents a city in the 'cities' table.
+    """
+    __tablename__ = 'cities'
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
-    # Create an engine and bind it to the Base class
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}', pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    # Create a session
-    Session = sessionmaker(b
+    state = relationship('State')
 
