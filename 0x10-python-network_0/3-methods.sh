645488de40fd55284aecda931701fdaa35fd86df
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 08:54:49 -0700

    d

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -I OPTIONS "$1" | grep "ALLOW:" | cut -f2- -d" "
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
