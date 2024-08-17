14b2fed13cd39a3417c5247bfc783defa7ed7621
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 09:03:01 -0700

    d

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI -X OPTIONS "$1" | grep -i "Allow:" | cut -d " " -f2- | tr ' ' ',' | sed 's/,$//'
+# Display all HTTP methods the server of a given URL will accept.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
