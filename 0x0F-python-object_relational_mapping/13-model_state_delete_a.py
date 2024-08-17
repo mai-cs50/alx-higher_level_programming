e03d74d24fdefd41dd347a009c4a306b26e653cf
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 08:22:42 -0700

    d

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python3
-'''
-write a script that takes in arguments and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
-But this time, write one that is safe from MySQL injections!
-'''
-
+"""
+Script that deletes all State objects with names containing the letter 'a'
+from the database hbtn_0e_6_usa.
+"""
 import sys
 from model_state import Base, State
-from SQLAlchemy import creat_engine
-from SQLAlchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = creat_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                          .format(argv[1], argv[2], argv[3]),
-                          pool_pre_ping=True)
+    if len(sys.argv) != 4:
+        print("Usage: ./13-model_state_delete_a.py <mysql username> "
+              "<mysql password> <database name>")
+        sys.exit(1)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Create the engine and session
+    engine = create_engine(
+        f"mysql+mysqldb://{mysql_username}:{mysql_password}@localhost/"
+        f"{database_name}",
+        pool_pre_ping=True
+    )
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.id == 2).all()
-    if states:
-        states[0].name = "New Mexico"
+
+    # Delete states where name contains the letter 'a'
+    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states_to_delete:
+        session.delete(state)
+    
     session.commit()
     session.close()
+
