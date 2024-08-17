220b3c549d7bfe5b951eb25b7ac99706671dd467
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 04:26:06 -0700

    d

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-'''
-defines a square by: (based on 2-square.py)
-'''
+# 3-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
 
 
 class Square:
-    '''
-    init
-    '''
-    def __init__(self, int):
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
