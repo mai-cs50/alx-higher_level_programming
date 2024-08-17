af7c022804e8df473d6b73f44ffba45630c97294
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 09:01:20 -0700

    d

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI -X OPTIONS "$1" | grep -i "Allow:" | cut -d " " -f2- | tr '\n' ',' | sed 's/,$//'
+curl -sI -X OPTIONS "$1" | grep -i "Allow:" | cut -d " " -f2- | tr ' ' ',' | sed 's/,$//'
