c296425712782e6719a6bd8a719959a703d1db34
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 08:26:00 -0700

    d

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -30,10 +30,10 @@ if __name__ == "__main__":
     session = Session()
 
     # Find all states where name contains the letter 'a'
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    states_to_delete = session.query(State).filter(
+            State.name.like('%a%')).all()
     for state in states_to_delete:
         session.delete(state)
 
     session.commit()
     session.close()
-
