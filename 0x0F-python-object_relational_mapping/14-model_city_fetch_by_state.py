d5e29b0616b9ca835f16587f462138261fa2eb46
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 08:38:15 -0700

    d

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./14-model_city_fetch_by_state.py <mysql username> <mysql password> <database name>")
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -22,16 +22,16 @@ if __name__ == "__main__":
     # Create an engine and bind it to the Base class
     engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}', pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    
+
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
     # Query to fetch all cities and their corresponding states
-    results = session.query(State, City).join(City).order_by(City.id).all()
+    results = session.query(City, State).join(State).order_by(City.id).all()
 
     # Print results in the required format
-    for state, city in results:
+    for city, state in results:
         print(f"{state.name}: ({city.id}) {city.name}")
 
     session.close()
