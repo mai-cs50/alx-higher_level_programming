2388e892cd6ae5610c152233a6f7cf59d263fd75
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 07:45:08 -0700

    d

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,6 +11,7 @@ from SQLAlchemy import creat_engine
 from SQLAlchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    #ddf fv
     engine = creat_engine('mysql+mysqldb://{}:{}@localhost/{}'
                           .format(argv[1], argv[2], argv[3]),
                           pool_pre_ping=True)
