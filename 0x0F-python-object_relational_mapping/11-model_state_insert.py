9cc58a45667cbcf777d5d26a28d01106bf4783e6
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 08:18:48 -0700

    d

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         f"{database_name}",
         pool_pre_ping=True
     )
-    
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
